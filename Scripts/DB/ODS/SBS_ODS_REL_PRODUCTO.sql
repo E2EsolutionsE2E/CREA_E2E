821de227e3ada0f3a15d20a6202dbac51c3265a7
Crishtian Mejia <cristhian.mejia@e2e.pe>
2017-07-03 10:41:18 -0500

    Funcionalidad actualizar e historico ODS_REL_PRODUCTO
    Creada tabla historica para sbs_ods_rel_producto y actualizado script
    para mantenimiento de taba sbs_ods_rel_producto

--- Scripts/DB/ODS/SBS_ODS_REL_PRODUCTO.sql
+++ Scripts/DB/ODS/SBS_ODS_REL_PRODUCTO.sql
@@ -1,11 +1,17 @@
-delete from sbs_ods_rel_producto where cod_periodo = @@p_cod_periodo@@;
+INSERT INTO SBS_ODS_REL_PRODUCTO_HIST (COD_CUENTA,COD_SUBPRODUCTO,TIP_CREDITO,COD_PERIODO)
+SELECT * FROM sbs_ods_rel_producto WHERE cod_periodo <> 201601--@@p_cod_periodo@@
+ON CONFLICT (COD_CUENTA,TIP_CREDITO,COD_PERIODO)DO NOTHING
+;
 
-@@@
+---@@@
+
+truncate sbs_ods_rel_producto;
 
 insert into sbs_ods_rel_producto 
 select
 cod_cuenta,
 cod_subproducto,
 tip_credito,
-@@p_cod_periodo@@ 
-from sbs_ods_rel_producto;
+201601--@@p_cod_periodo@@ 
+from sbs_stg_rel_producto
+;
